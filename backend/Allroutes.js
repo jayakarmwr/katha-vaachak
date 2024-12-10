d7b69b649b5e3373f14a05ee1dac32a1c634e76b
jayakarmwr <jayakarmwr@gmail.com>
2024-12-10 16:18:56 +0530

    story and images are getting stored

--- backend/Allroutes.js
+++ backend/Allroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {login,signup,confirmPassword,changePassword,getProfile}=require("../backend/controllers/get_set");
+const {login,signup,confirmPassword,changePassword,getProfile,savestory}=require("../backend/controllers/get_set");
 
 
 router.post("/getdata",login);
@@ -8,5 +8,6 @@ router.post("/change-password",changePassword);
 router.post("/signup",signup);
 router.post("/confirm",confirmPassword);
 router.get("/getuserdata",getProfile);
+router.post("/save-story",savestory);
 
 module.exports=router;
